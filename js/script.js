5942af1afcaa0e18c63db80d623bacf15ff3a860
Vadim <vadimm@citydom.ru>
2025-06-17 14:03:56 +0300

    added validator and change Sign in

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ window.onload = () => {
     const repPasswordEl = document.getElementsByName('repeatPassword')[0];
     const formBtn = document.querySelector('.form-button');
     const questionLinkEl = document.querySelector('.form-question-link');
+    const popup = document.querySelector('.popup');
 
     // 2. Запрещаем вводить все кроме букв и пробела в поле Full Name
     nameEl.addEventListener('input', function () {
@@ -64,6 +65,39 @@ window.onload = () => {
         };
     });
 
+    checkboxEl.addEventListener('change', function () {
+        if (this.checked) {
+            removeError(this.parentElement);
+        } else {
+            showError(this.parentElement, 'Необходимо принять соглашение');
+        };
+    });
+
+    function validator(corrector = 0) {
+        const inputEl = document.querySelectorAll('.form-input');
+        let valid = 0 - corrector;
+        for (let i = 0; i < inputEl.length; i++) {
+            // Проверка полей на пустое значение
+            if (!inputEl[i].value) {
+                inputEl[i].dispatchEvent(new Event('input'));
+            } else {
+                valid += 1;
+            };
+        };
+        // Проверка активации чекбокса
+        if (corrector === 0) {
+            if (!checkboxEl.checked) {
+                checkboxEl.dispatchEvent(new Event('change'));
+            } else {
+                valid += 1;
+            };
+        };
+        if (valid === inputEl.length + 1) {
+            return true;
+        };
+        return false;
+    }
+
     function showError(el, text) {
         if (el.classList.contains('error')) return;
         el.classList.add('error');
@@ -74,91 +108,62 @@ window.onload = () => {
             el.nextElementSibling.remove();
             el.classList.remove('error');
         };
-    }
-
-    // 5. Обработка нажатия кнопки Sign Up
-    const popup = document.querySelector('.popup');
-
-    formBtn.onclick = (event) => {
-        const inputEl = document.querySelectorAll('.form-input');
-        for (let i = 0; i < inputEl.length; i++) {
-            // Проверка полей на пустое значение
-            if (!inputEl[i].value &&
-                !(inputEl[i].name === 'password' || inputEl[i].name === 'repeatPassword')) {
-                switch (inputEl[i].name) {
-                    case 'fullName':
-                        alert('Введите Имя и Фамилию');
-                        break;
-                    case 'userName':
-                        alert('Введите Имя пользователя');
-                        break;
-                    case 'eMail':
-                        alert('Введите E-mail');
-                }
-                event.preventDefault();
-                return;
-            };
-            // Проверка поля ввода пароля
-            if (inputEl[i].name === 'password') {
-                if (inputEl[i].value.length < 8) {
-                    alert('Длинна пароля должна быть не менее 8 символов');
-                    event.preventDefault();
-                    return;
-                }
-            };
-            // Проверка подтверждения пароля
-            if (inputEl[i].name === 'repeatPassword') {
-                const passwordEl = document.getElementsByName('password')[0];
-                if (passwordEl.value !== inputEl[i].value) {
-                    alert('Подтвердите пароль');
-                    event.preventDefault();
-                    return;
-                }
-            };
-        };
-        // Проверка активации чекбокса
-        if (!checkboxEl.checked) {
-            alert('Примите соглашение');
-            event.preventDefault();
-            return;
-        };
-        event.preventDefault();                     // Отмена отправки формы
-        document.querySelector('.form').reset();    // Очистка формы
-        popup.classList.add('open');                // Открытие всплывающего окна
     };
+
+
+    // 5. Обработка нажатия кнопки Sign Up    
+    function onSignUp(e) {
+        e.preventDefault();
+        if (validator()) {
+            let base = localStorage.getItem('base');
+            const client = {};
+            client.name = nameEl.value;
+            client.user = userNameEl.value;
+            client.password = passwordEl.value;
+            if (base) {
+                let baseArr = JSON.parse(base);
+                baseArr.push(client);
+                localStorage.setItem('base', JSON.stringify(baseArr));
+            } else {
+                let baseArr = [];
+                baseArr.push(client);
+                localStorage.setItem('base', JSON.stringify(baseArr));
+            }
+            document.querySelector('.form').reset();    // Очистка формы
+            popup.classList.add('open');                // Открытие всплывающего окна
+        };
+    };
+    formBtn.addEventListener('click', onSignUp);
 
     // 6. Переход на Log In   
 
+    function onLogIn(e) {
+        e.preventDefault();
+        if (validator(1)) {
+            const base = JSON.parse(localStorage.getItem('base'));
+            for (let i = 0; i < base.length; i++) {
+                if (base[i].user === userNameEl.value && base[i].password === passwordEl.value) {
+                    console.log(1);
+                } else {
+                    console.log(0);
+                }
+            }
+
+        }
+    }
     // функция удаления и изменения элементов согласно ТЗ
     function transitionToLogIn() {
-        document.getElementsByClassName('main-form-title')[0].textContent = 'Log in to the system';
+        document.querySelector('.main-form-title').textContent = 'Log in to the system';
         nameEl.parentElement.remove();
         emailEl.parentElement.remove();
         repPasswordEl.parentElement.remove();
         checkboxEl.parentElement.remove();
-        questionLinkEl.remove();
+        questionLinkEl.textContent = 'Registration';
         formBtn.textContent = 'Sign In';
 
         // Замена обработчика клика на кнопке формы
-        formBtn.onclick = (event) => {
-            const inputEl = document.querySelectorAll('.form-input');
-            for (let i = 0; i < inputEl.length; i++) {
-                // Проверка полей на пустые значения
-                if (!inputEl[i].value) {
-                    switch (inputEl[i].name) {
-                        case 'userName':
-                            alert('Введите Имя пользователя');
-                            break;
-                        case 'password':
-                            alert('Введите пароль');
-                    }
-                    event.preventDefault();
-                    return;
-                }
-            };
-            alert(`Добро пожаловать, ${inputEl[0].value}!`);
-            event.preventDefault();
-        }
+        formBtn.removeEventListener('click', onSignUp);
+        formBtn.addEventListener('click', onLogIn);
     };
     // Вешаем функцию как обработчик клика на попап и ссылку в форме
     questionLinkEl.onclick = transitionToLogIn;
