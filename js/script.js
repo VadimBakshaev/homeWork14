d291b1dcc062a64ed29314f4581f0f2c8f1bd6ed
Vadim <vadimm@citydom.ru>
2025-06-18 14:04:28 +0300

    final edits and add comments

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// 1. Создаем обработчик события полной загрузки страницы
+// Создаем обработчик события полной загрузки страницы
 window.onload = () => {
     const nameEl = document.getElementsByName('fullName')[0];
     const userNameEl = document.getElementsByName('userName')[0];
@@ -9,8 +9,9 @@ window.onload = () => {
     const formBtn = document.querySelector('.form-button');
     const questionLinkEl = document.querySelector('.form-question-link');
     const popup = document.querySelector('.popup');
+    const titleEl = document.querySelector('.main-form-title');
 
-    // 2. Запрещаем вводить все кроме букв и пробела в поле Full Name
+    // Запрещаем вводить все кроме букв и пробела в поле Full Name
     nameEl.addEventListener('input', function () {
         this.value = this.value.replace(/[^a-zа-яё\s]/gi, '');
         if (!this.value) {
@@ -19,7 +20,8 @@ window.onload = () => {
             removeError(this);
         };
     });
-    // 3. Поле User Name может содержать только буквы, цифры, символ подчеркивания и тире
+
+    // Поле User Name может содержать только буквы, цифры, символ подчеркивания и тире
     userNameEl.addEventListener('input', function () {
         this.value = this.value.replace(/[^a-zа-яё\-_\d]/gi, '');
         if (!this.value) {
@@ -28,18 +30,20 @@ window.onload = () => {
             removeError(this);
         };
     });
-    // 4. Выводим сообщение в консоль об изменении состояния чекбокса
-    emailEl.addEventListener('input', isEmailValid);
-    function isEmailValid() {
+
+    // Проверяем правильность ввода е-мейла
+    emailEl.addEventListener('input', function () {
         if (this.value &&
             this.value.match(/[a-zа-яё\d\-\._]+@[a-zа-яё\d\-\._]+\.[a-zа-я]{2,3}/gi)) {
             removeError(this);
             return;
         };
-        showError(this, 'Введиет e-mail в формате email@mail.com');
-    };
+        showError(this, 'Введите e-mail в формате email@mail.com');
+    });
 
+    // Проверяем правильность ввода пароля
     passwordEl.addEventListener('input', function () {
+        // при изменении пароля, вызывам обработчик поля повторения пароля
         repPasswordEl.dispatchEvent(new Event('input'));
         if (this.value.length < 8) {
             showError(this, 'Пароль должен быть не менее 8 символов');
@@ -57,6 +61,7 @@ window.onload = () => {
         };
     });
 
+    // Проверям соответствие полей пароля
     repPasswordEl.addEventListener('input', function () {
         if (this.value === passwordEl.value) {
             removeError(this);
@@ -65,6 +70,7 @@ window.onload = () => {
         };
     });
 
+    // Проверяем состояние чекбокса
     checkboxEl.addEventListener('change', function () {
         if (this.checked) {
             removeError(this.parentElement);
@@ -73,9 +79,11 @@ window.onload = () => {
         };
     });
 
+    // Функция валидатор, так как на второй странице нет чекбокса, 
+    // ввел корректор для универсальности
     function validator(corrector = 0) {
         const inputEl = document.querySelectorAll('.form-input');
-        let valid = 0 - corrector;
+        let valid = 0 + corrector;
         for (let i = 0; i < inputEl.length; i++) {
             // Проверка полей на пустое значение
             if (!inputEl[i].value) {
@@ -98,11 +106,13 @@ window.onload = () => {
         return false;
     }
 
+    // Функция показывает сообщение об ошибке
     function showError(el, text) {
         if (el.classList.contains('error')) return;
         el.classList.add('error');
         el.insertAdjacentHTML('afterend', `<p class="error-message">${text}</p>`);
     };
+    // Функция убирает сообщение об ошибке
     function removeError(el) {
         if (el.classList.contains('error')) {
             el.nextElementSibling.remove();
@@ -110,16 +120,21 @@ window.onload = () => {
         };
     };
 
-
-    // 5. Обработка нажатия кнопки Sign Up    
+    // Обработка нажатия кнопки Sign Up (Регистрация)   
     function onSignUp(e) {
+        // Форму мы никуда не отправляем
         e.preventDefault();
+        // Если все поля заполнены
         if (validator()) {
-            let base = localStorage.getItem('base');
-            const client = {};
-            client.name = nameEl.value;
-            client.user = userNameEl.value;
-            client.password = passwordEl.value;
+            // получаем данные из хранилица
+            const base = localStorage.getItem('base');
+            // создаем объект с данными пользователя
+            const client = {
+                name : nameEl.value,
+                user : userNameEl.value,
+                password : passwordEl.value
+            };    
+            // добавляем пользователя в базу        
             if (base) {
                 let baseArr = JSON.parse(base);
                 baseArr.push(client);
@@ -129,31 +144,49 @@ window.onload = () => {
                 baseArr.push(client);
                 localStorage.setItem('base', JSON.stringify(baseArr));
             }
-            document.querySelector('.form').reset();    // Очистка формы
-            popup.classList.add('open');                // Открытие всплывающего окна
+            // очищаем форму
+            document.querySelector('.form').reset();    
+            popup.classList.add('open');            
         };
     };
+    // Вешаем обработчик на кнопку регистрации
     formBtn.addEventListener('click', onSignUp);
 
-    // 6. Переход на Log In   
+    // Переход на Sign In(Log In)   
 
+    // Обработка нажатия кнопки Sign In (Вход)
     function onLogIn(e) {
+        // Форму мы не отправляем
         e.preventDefault();
+        // Если поля заполнены
         if (validator(1)) {
+            // получаем данные
             const base = JSON.parse(localStorage.getItem('base'));
+            let flag = true;
+            // сверяем данные из полей с базой
             for (let i = 0; i < base.length; i++) {
-                if (base[i].user === userNameEl.value && base[i].password === passwordEl.value) {
-                    console.log(1);
-                } else {
-                    console.log(0);
-                }
-            }
+                if (base[i].user === userNameEl.value) {
+                    removeError(userNameEl);
+                    flag = false;
+                    if (base[i].password === passwordEl.value) {
+                        removeError(passwordEl);
+                        // если все данные введены правильно, переход в "ЛК"
+                        transitionToPersonalAccount(base[i]);
+                    } else {
+                        showError(passwordEl, 'Неверный пароль');
+                    };
+                };
+            };
+            // если пользователь не найден
+            if (flag) {
+                showError(userNameEl, `Пользователь ${userNameEl.value} не зарегистрирован`);
+            };
+        };
+    };
 
-        }
-    }
-    // функция удаления и изменения элементов согласно ТЗ
+    // функция "перехода" на страницу Log In
     function transitionToLogIn() {
-        document.querySelector('.main-form-title').textContent = 'Log in to the system';
+        titleEl.textContent = 'Log in to the system';
         nameEl.parentElement.remove();
         emailEl.parentElement.remove();
         repPasswordEl.parentElement.remove();
@@ -161,14 +194,48 @@ window.onload = () => {
         questionLinkEl.textContent = 'Registration';
         formBtn.textContent = 'Sign In';
 
-        // Замена обработчика клика на кнопке формы
+        // Замена обработчков
         formBtn.removeEventListener('click', onSignUp);
         formBtn.addEventListener('click', onLogIn);
+        questionLinkEl.removeEventListener('click', transitionToLogIn);
+        questionLinkEl.addEventListener('click', onReload);
+
     };
-    // Вешаем функцию как обработчик клика на попап и ссылку в форме
-    questionLinkEl.onclick = transitionToLogIn;
-    document.getElementsByClassName('popup-button')[0].onclick = () => {
+
+    // Функция "перехода" в личный кабинет
+    function transitionToPersonalAccount(user) {
+        titleEl.textContent = `Welcome, ${user.name}!`;
+        formBtn.removeEventListener('click', onLogIn);
+        formBtn.addEventListener('click', onReload);
+        formBtn.textContent = 'Exit';
+        userNameEl.parentElement.remove();
+        passwordEl.parentElement.remove();
+        questionLinkEl.remove();
+        document.querySelector('.main-form-text').remove();
+    };
+
+    // Функция перезагрузки страницы
+    function onReload() {
+        // Здесь начались "Чудеса". Почему-то location.reload() срабатывает по клику на ссылке
+        // но на кнопке Exit не работает. Все что меняется - это контекст, и даже после того
+        // как я сделал одинаковый контекст window, все равно, поведение осталось, я не понял почему так((
+        console.log('exit');  
+        // Пришлось изобретать "костыль":      
+        fetch(window.location.href).then(() => location.reload());
+
+        // Оставил варианты, что пробовал (на будущее)
+        //window.location.href = window.location.href + '?' + Date.now();
+        //window.location.replace(window.location.href);        
+        //location.reload();
+        // history.go(0);
+        // window.location.href = window.location.href;
+    };    
+
+    // Вешаем обработчик на клик по ссылке "Already have an account"
+    questionLinkEl.addEventListener('click', transitionToLogIn);
+    // Вешаем обработчик на кнопку ОК в модалке
+    document.querySelector('.popup-button').addEventListener('click', () => {
         popup.classList.remove('open');
         transitionToLogIn();
-    };
+    });
 }
